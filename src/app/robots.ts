314a3ab412287a6e4aad58e9f886f7b7b7726b5b
Ivan Romanov <omanov4@gmail.com>
2026-08-26 17:50:12 +0300

    убрал опечатки

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from next;
+import type { MetadataRoute } from "next";
 
-export default function robots() MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules {
-      userAgent ,
-      allow ,
+    rules: {
+      userAgent: "*",
+      allow: "/",
     },
-    sitemap httpsnuts-club.rusitemap.xml,
+    sitemap: "https://nuts-club.ru/sitemap.xml",
   };
 }
